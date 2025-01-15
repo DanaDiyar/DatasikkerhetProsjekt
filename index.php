e99015534a3969baa70b6befe2dc967ce0eef438
Joolrab <32496520+Joolrab@users.noreply.github.com>
2025-01-15 15:55:01 +0100

    HTML-kode

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Gjesteside</title>
+</head>
+<body>
+    <h1>Gjest – vis meldinger for emne</h1>
+
+    <form method="post">
+        Emnekode: <input type="text" name="subject_code" required>
+        PIN-kode: <input type="text" name="pin_code" required maxlength="4">
+        <input type="submit" value="Vis meldinger">
+    </form>
+    <hr>
