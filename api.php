5cbcd7568bf05de9776a98dc0216255c561e185c
DanaDiyar <finickydemize97@hotmail.com>
2025-01-21 13:23:11 +0100

    fjerna kommentarer

--- api.php
+++ api.php
@@ -1,14 +1,12 @@
 <?php
-// Inkluder databasekonfigurasjon
 include 'database.php';
 
-// Håndter forespørsler basert på 'action'-parameteren
 header("Content-Type: application/json");
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     if (isset($_GET['action'])) {
         if ($_GET['action'] === 'getMessages') {
-            // Hent meldinger
+        
             $sql = "SELECT * FROM messages";
             $result = $conn->query($sql);
             $messages = [];
@@ -17,7 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
             }
             echo json_encode($messages);
         } elseif ($_GET['action'] === 'reportMessage' && isset($_GET['messageId'])) {
-            // Marker melding som rapportert
+          
             $messageId = intval($_GET['messageId']);
             $sql = "UPDATE messages SET reported = 1 WHERE id = $messageId";
             if ($conn->query($sql) === TRUE) {
@@ -35,6 +33,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     echo json_encode(['error' => 'Ugyldig forespørsel']);
 }
 
-// Lukk databaseforbindelse
 $conn->close();
 ?>
