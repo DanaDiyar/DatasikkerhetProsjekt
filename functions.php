441dddf71738af36135e652751dc69f5d070a279
miran-nihad <miran.nihad@hotmail.com>
2025-01-22 13:00:25 +0100

    laget funksjon funksjonalitet

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+function sanitize($data) {
+    return htmlspecialchars(strip_tags(trim($data)));
+}
+
+function hashPassword($password) {
+    return password_hash($password, PASSWORD_BCRYPT);
+}
+
+function verifyPassword($password, $hash) {
+    return password_verify($password, $hash);
+}
+?>
