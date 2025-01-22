78d6abf14ddebfc7038537c521e05902c4960ec0
DanaDiyar <finickydemize97@hotmail.com>
2025-01-22 14:39:00 +0100

    dokumentasjon + viktige endringer

--- api.php
+++ api.php
@@ -33,5 +33,35 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     echo json_encode(['error' => 'Ugyldig forespørsel']);
 }
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if ($_POST['action'] === 'changePassword') {
+        $username = $_POST['username'];
+        $oldPassword = $_POST['oldPassword'];
+        $newPassword = $_POST['newPassword'];
+
+        $sql = "SELECT * FROM teacher WHERE username = ? AND password = MD5(?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("ss", $username, $oldPassword);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows > 0) {
+
+            $updateSql = "UPDATE teacher SET password = MD5(?) WHERE username = ?";
+            $updateStmt = $conn->prepare($updateSql);
+            $updateStmt->bind_param("ss", $newPassword, $username);
+            $updateStmt->execute();
+
+            echo json_encode(['status' => 'success', 'message' => 'Passordet er endret.']);
+        } else {
+            echo json_encode(['status' => 'error', 'message' => 'Feil brukernavn eller passord.']);
+        }
+    } else {
+        echo json_encode(['error' => 'Ugyldig handling.']);
+    }
+} else {
+    echo json_encode(['error' => 'Ugyldig forespørsel.']);
+}
+
 $conn->close();
 ?>
--- database/database.php
+++ database/database.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "password";
+$password = "passord";
 $dbname = "studentapp";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
