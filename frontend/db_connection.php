f1c716d23611a025e3407d4608fca10ea7931b26
Ahmedmuse12 <111729465+Ahmedmuse12@users.noreply.github.com>
2025-02-12 15:42:01 +0100

    Update db_connection.php

--- frontend/db_connection.php
+++ frontend/db_connection.php
@@ -1,12 +1,14 @@
 <?php
-$host = '158.39.188.205';
-$dbname = 'Datasikkerhet';
-$username = 'datasikkerhet';
-$password = 'DittPassord';
+$host = "158.39.188.205"; // Din MySQL-server IP
+$dbname = "Datasikkerhet"; // Ditt databasenavn
+$username = "datasikkerhet"; // MySQL-brukernavn
+$password = "DittPassord"; // MySQL-passord
 
+// Koble til databasen
 $conn = new mysqli($host, $username, $password, $dbname);
 
+// Sjekk om tilkoblingen er vellykket
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Tilkoblingsfeil: " . $conn->connect_error);
 }
 ?>
