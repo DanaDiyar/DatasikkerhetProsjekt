d2f8a7c31c1f2d4eaf89acd0520c1d206777bbb9
miran-nihad <miran.nihad@hotmail.com>
2025-02-12 15:22:41 +0100

    fiks

--- meldingssystem.php
+++ meldingssystem.php
@@ -1,12 +1,16 @@
 <?php
-$host = '158.39.188.205'; // Eller IP-adressen til databasen
+$host = '158.39.188.205';
 $dbname = 'Datasikkerhet';
-$username = 'datasikkerhet'; // Sett ditt MySQL-brukernavn
-$password = 'DittPassord'; // Sett passordet ditt (hvis det er satt)
+$username = 'datasikkerhet';
+$password = 'DittPassord';
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ]);
+    echo "Tilkobling vellykket!";
 } catch (PDOException $e) {
     die("Feil ved tilkobling til database: " . $e->getMessage());
 }
