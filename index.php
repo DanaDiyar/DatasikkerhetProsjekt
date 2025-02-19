f4cd801c5cbdf6868ed3416a0947897c8bdc6068
Joolrab <32496520+Joolrab@users.noreply.github.com>
2025-02-19 14:41:05 +0100

    Andre forsøk på nedtrekksliste for emnekode

--- index.php
+++ index.php
@@ -16,6 +16,30 @@ if (isset($_POST['comment_text']) && isset($_POST['message_id'])) {
     echo "Kommentar er lagt til!";
 }
 
+// Hent studentinfo fra sesjonen
+$student_id = $_SESSION['user_id'];
+$student_email = $_SESSION['user_email'];
+$student_navn = $_SESSION['user_name'];
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// Hent emner studenten er påmeldt til (hvis aktuelt)
+$subjects = [];
+$query = "SELECT id, emnenavn, foreleser_id FROM emner";
+$result = $conn->query($query);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $subjects[$row['id']] = [
+            'name' => $row['emnenavn'],
+            'foreleser_id' => $row['foreleser_id']
+        ];
+    }
+} else {
+    $subjects[0] = ['name' => "Ingen emner funnet", 'foreleser_id' => null];
+}
+
 /* Håndtering av innlogging for emne */
 if (isset($_POST['subject_code']) && isset($_POST['pin_code'])) {
     $subjectCodeInput = $_POST['subject_code'];
